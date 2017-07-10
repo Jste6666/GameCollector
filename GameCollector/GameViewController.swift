380fa259ccd083a5b5c8ce6a6207c323bbbf7aa1
Justin Stewart <discgolfstew@gmail.com>
2017-07-10 05:04:08 -0400

    finished section 4

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Justin Stewart on 7/10/17.
+//  Copyright © 2017 Justin Stewart. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
